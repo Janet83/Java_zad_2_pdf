a9dc97c185cabfbaf9e5b28cbb924be9f1b66c2f
Janet <zan.smigiel@gmail.com>
2022-05-25 18:28:11 +0200

    Java_20220327 - Zadanie 2

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class Animal {
+    final String species;
+    public Double weight;
+    boolean isAlive;
+    String name;
+
+    public Animal(String species) {
+        this.isAlive = true;
+        this.species = species;
+
+        switch (this.species) {
+            case "felis":
+                this.weight = 5.0;
+                break;
+            case "canis":
+                this.weight = 2.0;
+                break;
+            default:
+                this.weight = 10.0;
+                break;
+        }
+    }
+
+    public void feed() {
+        if (isAlive) {
+            weight += 1.0;
+            System.out.println("Thx for food");
+        } else {
+            System.out.println("Trochę za późno");
+        }
+    }
+
+    public void takeForAWalk() {
+        if (isAlive) {
+            weight -= 1.0;
+            System.out.println("Nice walk, thx");
+            if (weight <= 0.0) {
+                isAlive = false;
+            }
+        } else {
+            System.out.println("Halo policja, ktoś targa martwe zwierzę po chodniku");
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Animal dog = new Animal("canis");
+        dog.isAlive = true;
+        dog.name = "Szarik";
+
+        System.out.println(dog.name);
+        System.out.println("species: " + dog.species + " name: " + dog.name);
+
+        dog.feed();
+        dog.feed();
+        dog.feed();
+
+        System.out.println("waga po karmieniu: " + dog.weight);
+
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+
+        System.out.println("waga po spacerze: " + dog.weight);
+
+
+        Car volvo = new Car();
     }
 }
