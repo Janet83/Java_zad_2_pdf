b403fa792c8879097482f8eb51b753475d64a922
Janet <zan.smigiel@gmail.com>
2022-05-25 18:35:31 +0200

    Java_20220327 - Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,4 +3,20 @@ package com.company;
 public class Human {
     Animal pet;
     Car volvo;
+    private Double salary;
+
+    public Double getSalary() {
+        System.out.println("Dane o wypłacie były pobrane " + java.time.LocalDateTime.now());
+        return this.salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary < 0.0) {
+            System.out.println("Chyba Cię powaliło, nie ma niewolnictwa");
+        }
+        System.out.println("Nowa wypłata wysłana do systemu księgowego");
+        System.out.println("Odbierz aneks od Pani Hani");
+        System.out.println("Nie ukrywaj dochodów, ZUS i US i tak Cię znajdą");
+        this.salary = salary;
+    }
 }
